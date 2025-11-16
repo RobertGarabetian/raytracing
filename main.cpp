49acf630d21d56b775a093adf8223c10f51c4eb8
RobertGarabetian <robertgarabetian90@gmail.com>
2025-11-15 18:45:09 -0800

    adding ray class and creating camera and viewport

--- main.cpp
+++ main.cpp
@@ -1,26 +1,55 @@
-#include <iostream>
 #include "vec3.h"
+#include "color.h"
+#include "ray.h"
+
+#include <iostream>
+
+color ray_color(const ray& r) {
+    return color(0,0,0);
+}
 int main() {
-    int image_width = 256;
-    int image_height = 256;
-    int ir, ig, ib;
+
+    auto aspect_ration = 16.0 / 9.0;
+    int image_width = 400;
+
+    //Calculate the image height and ensure that it's at least 1.
+    int image_height = int(image_width / aspect_ration);
+    if (image_height < 1) {
+        image_height = 1;
+    }
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto camera_center = point3(0,0,0);
+
+    //viewport vectors
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    //delta vectors
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    //upper left pixel
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel100_location = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    //render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            double r = double(i) / (image_width - 1);
-            double g = double(j) / (image_height - 1);
-            double b = 0.0;
+            auto pixel_center = pixel100_location + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
 
-            ir = int(255.999 * r);
-            ig = int(255.999 * g);
-            ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            ray r(camera_center, ray_direction);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
         }
     }
 
-    std::clog << ir << ' ' << ig << ' ' << ib << '\n';
+    std::clog << "\rDone.                   \n";
     return 0;
 }
